58c61078fffd1e4c460ef2922f7e2175021573f8
Robi <ciobanurobert100@gmail.com>
2024-04-03 01:20:27 +0300

    added chart to dashboard

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuBarModule } from '../../menu-bar/menu-bar.module';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Chart, Legend } from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -13,6 +14,60 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit{
+  public prices: any;
 
+  createChart() {
+    this.prices = new Chart('prices', {
+      type: 'line',
+
+      data: {
+        labels: ['2020' , '2021', '2022', '2023', '2024'],
+
+        datasets: [
+          {
+            label: "product1",
+            data: ['70', '32', '23', '34', '38'],
+            backgroundColor: 'rgb(23, 165, 20)',
+            borderColor: 'rgb(23, 165, 20)',
+          },
+          {
+            label: "product2",
+            data: ['43', '21', '54', '75', '80'],
+            backgroundColor: 'rgb(20, 43, 165)',
+            borderColor: 'rgb(20, 43, 165)',
+          },
+          {
+            label: "product3",
+            data: ['43', '54', '67', '56', '40'],
+            backgroundColor: 'rgb(156, 20, 24)',
+            borderColor: 'rgb(156, 20, 24)',
+          },
+        ]
+      },
+      options: {
+        scales: {
+          x: {
+            grid: {
+              display: false,
+            },
+          },
+          y: {
+            grid: {
+              display: false,
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      }
+    });
+  }
+
+  ngOnInit(): void {
+      this.createChart();
+  }
 }
