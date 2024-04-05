0c1d7700af238a183516b836ebe9274201c1be31
Robi <ciobanurobert100@gmail.com>
2024-04-05 20:42:40 +0300

    added mongodb

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,11 +1,31 @@
 const http = require('http');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app);
+const MONG_URL = process.env.MONGO;
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
+const server = http.createServer(app) ;
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
 });
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
+async function startServer() {
+    await mongoose.connect(MONG_URL);
+
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+}
+
+startServer()
