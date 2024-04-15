dc7a966531f629440753125c31d1906486fbc39f
Robi <ciobanurobert100@gmail.com>
2024-04-16 01:27:16 +0300

    updapted settings

--- Server/public/server/main.server.mjs
+++ Server/public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WWGCQHT4.mjs";import"./chunk-TJYUVH2T.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-VYPCEVZN.mjs";import"./chunk-TJYUVH2T.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
