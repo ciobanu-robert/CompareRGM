f2f52095be586a6aef74cec9c8aaa4d6c671be53
Robi <ciobanurobert100@gmail.com>
2024-04-16 01:02:45 +0300

    updated terms page + changes to statistics

--- Client/src/app/pages/terms-page/terms-page.component.ts
+++ Client/src/app/pages/terms-page/terms-page.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuBarModule } from '../../menu-bar/menu-bar.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-terms-page',
   standalone: true,
   imports: [
     MenuBarModule,
+    CommonModule,
   ],
   templateUrl: './terms-page.component.html',
   styleUrl: './terms-page.component.css'
 })
-export class TermsPageComponent {
+export class TermsPageComponent implements OnInit{
+  localStorageLenght = localStorage.length;
+  disabled = true;
 
+  ngOnInit() {
+    if (this.localStorageLenght > 1) {
+      this.disabled = false;
+    } else {
+      this.disabled = true;
+    }
+  }
 }
--- Server/public/server/main.server.mjs
+++ Server/public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VII2FJN4.mjs";import"./chunk-TJYUVH2T.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-4KNOGRK3.mjs";import"./chunk-TJYUVH2T.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
