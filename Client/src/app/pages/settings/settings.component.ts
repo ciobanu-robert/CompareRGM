f066f8f3208bc6398fc21681b23e3e84be0fc811
Robi <ciobanurobert100@gmail.com>
2024-04-16 19:13:16 +0300

    dashboard + settings update

--- Client/src/app/pages/settings/settings.component.ts
+++ Client/src/app/pages/settings/settings.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MenuBarModule } from '../../menu-bar/menu-bar.module';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import {  FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatchValidatorService } from '../../services/match-validator.service';
+import { InputComponent } from '../../shared/input/input.component';
 
 @Component({
   selector: 'app-settings',
@@ -9,11 +12,39 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MenuBarModule,
     RouterLink,
     RouterLinkActive,
+    ReactiveFormsModule,
+    InputComponent,
   ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  constructor(private matchValidator: MatchValidatorService) {}
+  newPasswordTipe = 'password';
+  confirmNewPasswordTipe = 'password';
+
+  newImage = new FormControl('');
+  newCompany = new FormControl('',[
+    Validators.minLength(3),
+  ]);
+  newDescription = new FormControl('' , [
+    Validators.maxLength(271),
+  ]);
+  newPassword = new FormControl('', [
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
+    this.matchValidator.match('confirmNewPassword', true),
+  ]);
+  confirmNewPassword = new FormControl('', [
+    this.matchValidator.match('newPassword')
+  ]);
+
+  settingsForm = new FormGroup({
+    newImage: this.newImage,
+    newCompany: this.newCompany,
+    newDescription: this.newDescription,
+    newPassword: this.newPassword,
+    confirmNewPassword: this.confirmNewPassword,
+  });
 
   limitTextArea(e: KeyboardEvent) {
     let textarea = e.target as any;
@@ -27,5 +58,9 @@ export class SettingsComponent {
       .join('\n');
 
     textarea.value = newContent;
+  }
+
+  save() {
+
   }
 }
--- Server/public/server/main.server.mjs
+++ Server/public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VYPCEVZN.mjs";import"./chunk-TJYUVH2T.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-PZKSJQX5.mjs";import"./chunk-TJYUVH2T.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
