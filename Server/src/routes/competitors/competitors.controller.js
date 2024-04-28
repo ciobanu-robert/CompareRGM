a94b13f545129af719675a374ac42c56be513c5b
Robi <ciobanurobert100@gmail.com>
2024-04-28 16:51:27 +0300

    updated competitors cotroller

--- Server/src/routes/competitors/competitors.controller.js
+++ Server/src/routes/competitors/competitors.controller.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+ObjectID = require('mongoose').ObjectID;
 
 const User = require('../../models/user.mongoose');
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -9,13 +10,21 @@ async function httpGetCompetitors(req, res) {
     const { token } = req.body;
 
     try {
-        const user = jwt.verify(token, JWT_SECRET);
+        const _user = jwt.verify(token, JWT_SECRET);
+        const user = await User.findById(_user.id);
+        const negateUsers = [_user.id];
+        const competitorsInvites = await User.find();
 
-        competitors = await User.find({_id: {$ne: user.id}}).select({
+        for (let notification of user.notifications) {
+            negateUsers.push(notification.competitorID);
+        }
+
+        const competitors = await User.find({_id: {'$nin': negateUsers}}).select({
             password: 0,
             notifications: 0,
             competitors: 0,
         });
+
         res.json({ status: 'ok', data: competitors });
     } catch {
         res.json({ status: 'error', error: 'Something went wrong' })
