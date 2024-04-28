ac9681a8177b6b473e7e01f07291162c3049cc00
Robi <ciobanurobert100@gmail.com>
2024-04-28 20:15:30 +0300

    modified competitors page

--- Server/public/server/main.server.mjs
+++ Server/public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QXDTGMW3.mjs";import"./chunk-ISBZMXRL.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-NM76DCAG.mjs";import"./chunk-ISBZMXRL.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
